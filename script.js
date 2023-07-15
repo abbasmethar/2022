bb9cc76a03678d34b68725ecd7195cf5fdddd4b7
Abbas Methar <abbasmethar2000@gmail.com>
2023-07-15 21:35:57 +0530

    added mail submission

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ $(document).ready(function(){
         }
     })
   })
+
+  
+  $("#submit-form").submit((e)=>{
+      e.preventDefault()
+      $.ajax({
+          url:"https://script.google.com/macros/s/AKfycbwXXcesDs8FZRAxgCGs1Vd7LXii9VSoCrQTr5N6mMzhcClIMZVVlqtKs1WnBD7dNIxf/exec",
+          data:$("#submit-form").serialize(),
+          method:"post",
+          success:function (response){
+              alert("Form submitted successfully")
+              window.location.reload()
+              //window.location.href="https://google.com"
+          },
+          error:function (err){
+              alert("Something Error")
+
+          }
+      })
+  })
